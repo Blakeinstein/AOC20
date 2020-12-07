6aa5225950f00d04d71023a6aefc8cfbbcab5946
Blaine <rishi1998@gmail.com>
2020-12-08 02:05:24 +0530

    add template

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main() {
+    ifstream input_data("inputs/template.txt");
+    string input;
+    while(getline(input_data, input))
+        cout << input << endl;
+        
+    return 0;
+}
